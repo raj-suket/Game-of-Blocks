8a2bedc208a49f744d8b435e02407071712dc119
raj-suket <suketraj20@iitk.ac.in>
2021-05-24 01:11:09 +0530

    Initial Commit

---
+++ Assignment1/main.py
@@ -0,0 +1,26 @@
+from Crypto.Hash import SHA256
+import time
+
+h=SHA256.new()
+s=input("Enter the string: ")
+begin=time.time()
+res = bytes(s, 'utf-8')
+i=1
+while True:
+    news=res
+    hn=SHA256.new()
+    hn.update(res)
+    x=str(i)
+    res1=bytes(x, 'utf-8')
+    hn.update(res1)
+    ans=hn.hexdigest()
+    ans=int(ans, 16)
+    if ans<0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF :
+        time.sleep(1)
+        end=time.time()
+        print(f'\nThe required positive number is : {i}')
+        break
+    
+    i=i+1
+
+print(f"\nExecuted with total time {end - begin} seconds")
